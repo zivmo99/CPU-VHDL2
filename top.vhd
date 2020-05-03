abb2d62e9f47eec3006a91241d8936baa58ec390
orshicohen <64729077+orshicohen@users.noreply.github.com>
2020-05-03 21:42:55 +0300

    Update top.vhd

--- top.vhd
+++ top.vhd
@@ -2,6 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE work.aux_package.all;
+USE ieee.numeric_std.ALL;
 -------------------------------------------------------------
 entity top is
 	generic (
@@ -18,11 +19,47 @@ entity top is
 end top;
 ------------- complete the top Architecture code --------------
 architecture arc_sys of top is
-	
+signal ding_new: STD_LOGIC_VECTOR (n-1 DOWNTO 0);
+signal din_old: STD_LOGIC_VECTOR (n-1 DOWNTO 0);
+SIGNAL din_old_xor : std_logic_vector(n-1 DOWNTO 0);
+signal sub : std_logic_vector(n DOWNTO 0);
+signal subInt : integer;
+signal rise: STD_LOGIC;
+
 begin
+	process(rst,clk,ena)
+	begin
+	   if(rst='1') then
+		    din_new<= (others=>'0');
+		    din_old<=  (others=>'0');
+	   elsif (clk'EVENT AND clk='1') then
+	      if(ena='1') then
+     			  din_new<=din;
+     			  din_old<=dnew;
+		    end if;
+	   end if;
+	end process;	
 	
-				
-	L0 : Adder generic map(n) port map(); -- Adder single instantiation
+		   
+	temp : for i in 0 to n-1 generate
+			din_old_xor(i) <= (not(din_old(i)));
+	end generate;
+
+
+	L0 : Adder generic map(n) port map(
+		a=>din_new,
+		b=>din_old_xor,
+		cin=>'1',
+		s=>sub(n-1 DOWNTO 0),
+		cout=>sub(n)
+	); -- Adder single instantiation
+	
+	subInt <= to_integer(unsigned(sub(n-1 DOWNTO 0)));
+	rise <= '1' when cond=0 AND subInt=1 else
+		'1' when cond=1 AND subInt=2 else
+		'1' when cond=2 AND subInt=3 else
+		'1' when cond=3 AND subInt=4 else
+		'0';
 	
 	
 end arc_sys;
